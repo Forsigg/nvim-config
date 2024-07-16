9c97c1d51192ce0f340708a08e17b2f2af09f799
forsigg <al.berhmiller@gmail.com>
2024-07-16 12:10:49 +0700

    Add after dir & add config for dashboard

---
+++ after/ftplugin/dashboard.lua
@@ -0,0 +1,7 @@
+-- vim.opt.shiftwidth = 
+-- vim.opt.tabstop = 0
+-- vim.opt.softtabstop = 0
+vim.opt.smartindent = false
+vim.opt.expandtab = false
+vim.wo.number = false
+vim.wo.relativenumber = false
