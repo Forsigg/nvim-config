f2da29775dd3b033412c6694b4902b02c8e6a8dc
forsigg <al.berhmiller@gmail.com>
2024-07-16 11:50:24 +0700

    Update config

--- lua/core/configs.lua
+++ lua/core/configs.lua
@@ -1,5 +1,5 @@
-vim.wo.number = true
-vim.wo.relativenumber = true
+-- vim.wo.number = true
+-- vim.wo.relativenumber = true
 
 vim.g.did_load_filetypes = 1
 vim.g.formatoptions = "qrn1"
@@ -11,7 +11,7 @@ vim.opt.wrap = false
 vim.wo.linebreak = true
 vim.opt.virtualedit = "block"
 vim.opt.undofile = true
-vim.opt.shell = "/bin/bash"
+vim.opt.shell = "/bin/zsh"
 
 -- Mouse
 vim.opt.mouse = "a"
@@ -46,14 +46,14 @@ vim.opt.foldenable = false
 
 -- Fillchars
 vim.opt.fillchars = {
-    vert = "│",
-    fold = "⠀",
-    eob = " ", -- suppress ~ at EndOfBuffer
-    -- diff = "⣿", -- alternatives = ⣿ ░ ─ ╱
-    msgsep = "‾",
-    foldopen = "▾",
-    foldsep = "│",
-    foldclose = "▸"
+  vert = "│",
+  fold = "⠀",
+  eob = " ", -- suppress ~ at EndOfBuffer
+  -- diff = "⣿", -- alternatives = ⣿ ░ ─ ╱
+  msgsep = "‾",
+  foldopen = "▾",
+  foldsep = "│",
+  foldclose = "▸"
 }
 
 vim.cmd([[highlight clear LineNr]])
@@ -61,12 +61,54 @@ vim.cmd([[highlight clear SignColumn]])
 
 -- Resolve missing line numbers error
 vim.api.nvim_create_autocmd(
-    "BufEnter",
-    {
-        pattern = "*",
-        callback = function()
-            vim.opt_local.relativenumber = true
-            vim.opt_local.number = true
-        end
-    }
+  "BufEnter",
+  {
+    pattern = "*",
+    callback = function()
+      if vim.bo.filetype == "dashboad" then
+        vim.opt_local.relativenumber = false
+        vim.opt_local.number = false
+        return
+      end
+      vim.opt_local.relativenumber = true
+      vim.opt_local.number = true
+    end
+  }
+)
+
+-- Auto set indent by filetype
+vim.api.nvim_create_autocmd(
+  "BufEnter",
+  {
+    pattern = "*",
+    callback = function()
+      local filetype = vim.bo.filetype
+      if filetype == "dashboad" then
+        vim.opt.shiftwidth = 0
+        vim.opt.tabstop = 0
+        vim.opt.softtabstop = 0
+        vim.opt.smartindent = false
+        vim.opt.expandtab = false
+        vim.wo.number = false
+        vim.wo.relativenumber = false
+        return
+      end
+
+      vim.wo.number = true
+      vim.wo.relativenumber = true
+
+      if filetype == "python" or filetype == "go" then
+        vim.opt.shiftwidth = 4
+        vim.opt.tabstop = 4
+        vim.opt.softtabstop = 4
+        vim.opt.smartindent = true
+        return
+      end
+
+      vim.opt.shiftwidth = 2
+      vim.opt.tabstop = 2
+      vim.opt.softtabstop = 2
+      vim.opt.smartindent = true
+    end
+  }
 )
